249c58473843373c292339f7f4ae00c7217e281e
mstasinski <mstasinski@clickmeeting.com>
2025-04-05 11:59:46 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthButtonsComponent } from './navbar/components/auth-buttons/auth-buttons.component';
+import { NavbarContainerComponent } from "./navbar/containers/navbar-container/navbar-container.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
