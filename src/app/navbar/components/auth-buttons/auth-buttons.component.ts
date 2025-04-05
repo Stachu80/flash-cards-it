a6322500e4f8edf0e82d33ee5535c8a189053e95
mstasinski <mstasinski@clickmeeting.com>
2025-04-05 12:02:26 +0200

    init

---
+++ src/app/navbar/components/auth-buttons/auth-buttons.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-buttons',
+  imports: [],
+  templateUrl: './auth-buttons.component.html',
+  styleUrl: './auth-buttons.component.scss'
+})
+export class AuthButtonsComponent {
+
+}
---
+++ src/app/navbar/containers/navbar-container/navbar-container.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-container',
+  imports: [],
+  templateUrl: './navbar-container.component.html',
+  styleUrl: './navbar-container.component.scss'
+})
+export class NavbarContainerComponent {
+
+
+  showSingInModal = () => { }
+}
